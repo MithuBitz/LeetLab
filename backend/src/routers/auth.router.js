e25405a8da30f1a3e49df29c7a737e2bc40fa3c7
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2025-04-29 18:05:59 +0530

    add logout controller and router with a authmiddleware

--- backend/src/routers/auth.router.js
+++ backend/src/routers/auth.router.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, registerUser } from "../controllers/auth.controller.js";
+import { login, logout, registerUser } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", login);
+router.get("/logout", authMiddleware, logout);
 
 export default router;
