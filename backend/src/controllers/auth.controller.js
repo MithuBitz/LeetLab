894653fbac7b29a4cba631aab53f700966ec2453
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2025-04-29 16:45:03 +0530

    initialize the auth controller and routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const registerUser = async (req, res) => {
+  res.send("Register controller hit");
+};
+
+export const login = async (req, res) => {
+  res.send("Login controller hit");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout controller hit");
+};
+
+export const check = async (req, res) => {
+  res.send("Check controller hit");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routers/auth.router.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//Enable cookies
+app.use(cookieParser());
+
+//Rotes
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
