a07c2ce1ffb9e17b3242daadaa194ff0807a0127
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2025-05-02 16:29:01 +0530

    almost complete the createProblem controller

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -3,39 +3,79 @@ import { getJudge0LanguageId, submitBatch } from "../libs/judge0.libs.js";
 
 export const createProblem = async (req, res) => {
   // res.send("Problem controller hit");
-  const { title, description, difficulty, tags, examples, constraints, testcases, codeSnippets, referenceSolutions } = req.body;
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
 
-  if(req.user.role !== "ADMIN") {
-    return res.status(403).json({ message: "Unauthorized - Access denied you are not an admin" });
+  if (req.user.role !== "ADMIN") {
+    return res
+      .status(403)
+      .json({ message: "Unauthorized - Access denied you are not an admin" });
   }
 
   try {
     // get all the language and solutionCode from the refferenceSolutions object
-    for(const [language, solutionCode] of Object.entries(referenceSolutions)) {
+    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
       //Get the judge0 language id of the respactive language
       const languageId = await getJudge0LanguageId(language);
 
-      if(languageId == null) {
-        return res.status(400).json({ message: `Language ${language} not supported` });
+      if (languageId == null) {
+        return res
+          .status(400)
+          .json({ message: `Language ${language} not supported` });
       }
       //We have to create a submission for each testcase
-      const submission = testcases.map(({input, output}) => ({
-        source_code : solutionCode,
-        language_id : languageId,
-        stdin : input,
-        expacted_output : output
-      }))
+      const submission = testcases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expacted_output: output,
+      }));
 
       const submissionResult = await submitBatch(submission);
 
       //Extract the token from the submissionResultcr
-      const token = submissionResult.map((result) => result.token)
+      const token = submissionResult.map((result) => result.token);
 
-     const result = await pollBatchResult(token);
+      const results = await pollBatchResult(token);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        console.log(result);
+
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            error: `Testcase ${i + 1} failed for language ${language}`,
+          });
+        }
+      }
+      // Create and save a problem
+      const newProblem = await db.problem.create({
+        data: {
+          title,
+          description,
+          difficulty,
+          tags,
+          examples,
+          constraints,
+          testcases,
+          codeSnippets,
+          referenceSolutions,
+          userId: req.user.id,
+        },
+      });
+
+      return res.status(201).json(newProblem);
     }
-  } catch (error) {
-    
-  }
+  } catch (error) {}
 };
 
 export const getAllProblems = async (req, res) => {
