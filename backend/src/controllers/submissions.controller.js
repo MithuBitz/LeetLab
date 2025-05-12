e33719c7f642adf022cc9e4de2ebf1cbc45d178c
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2025-05-12 22:15:57 +0530

    complete getAllSubmission controller

--- backend/src/controllers/submissions.controller.js
+++ backend/src/controllers/submissions.controller.js
@@ -1,5 +1,27 @@
+import { db } from "../libs/db.js";
+
 export const getAllSubmissions = async (req, res) => {
   //   console.log(" 🎮 Get All submission controller Hit");
+  try {
+    const userId = req.user.id;
+    const submissions = await db.submission.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Submissions fetched successfully",
+      submissions,
+    });
+  } catch (error) {
+    console.error("Error executing code:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error while executing code",
+    });
+  }
 };
 
 export const getSubmissionById = async (req, res) => {
